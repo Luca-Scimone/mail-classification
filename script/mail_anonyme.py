c3bd3e69bcafdad251edaf324c929eab61ace584
CORIAT BASTIEN <bastien.coriat@etu.unistra.fr>
2021-03-08 00:30:20 +0100

    Fix some regex & warnings

--- script/mail_anonyme.py
+++ script/mail_anonyme.py
@@ -1,22 +1,26 @@
-# This is a sample Python script.
+"""
+Ce script permet d'anonymiser les mails pour le projet ingénieur.
+Il s'appelle comme suit : python3 mail_anonyme.py --file f
+Il lit alors le fichier f, extrait les mails et les anonymise (remplace par exemple les prénoms,
+numéro de téléphone...). Il écrit les mails anonymisés dans un fichier output. Le destinataire et l'émetteur du mail
+sont hashés. La correspondance strinh/hash_string est écrite dans un fichier 'secret'.
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+Pour un directory : python3 mail_anonyme.py --dir d --rec
+"""
 
 import hashlib
-import locale
 import os
 import re
 from typing import TextIO
 import argparse
 
 # ajouter un mot de civilité si non pris en compte. Attention Madame et madame ne sont pas équivalents.
-CIVILITE = "(madame|Madame|monsieur|Monsieur|mr|Mr|mme|Mme|melle|Mlle)\s*.\s*"
+CIVILITE = r"(?:madame|Madame|monsieur|Monsieur|mr|Mr|mme|Mme|melle|Mlle|M|m)\s*.\s*"
 
 # ajouter une formule si manquante. Insensible à la casse (Cordialement et cordialement sont équivalents)
-END_OF_MAIL = "(cordialement|cdt|amicalement|sincèrement|sincère salutation|bien cordialement|bien " \
-              "sincèrement|cordialement " \
-              "vôtre|bien à vous|avec mes salutations)[,.;]?\s*[,.;]?\s*"
+END_OF_MAIL = r"(cordialement|cdt|amicalement|sincèrement|sincère salutation|bien cordialement|bien " \
+              r"sincèrement|cordialement " \
+              r"vôtre|bien à vous|avec mes salutations)[,.;]?\s*[,.;]?\s*"
 # Cette chaine définit par quoi sera remplacé les prénoms-noms. Attentions, les nombres et l'expéditeur ne sont
 # pas concernés.
 ANONYME = ' anonyme-anonyme '
@@ -27,10 +31,12 @@ ENCODE_WRITE = 'utf8'
 # Don't edit bellow
 # ---------------------------------------------------------------------------------- #
 REGEX_MAIL = r'[^\s]*@[^\s]*'
-NAME_RE = r'(?:[A-Z][A-Za-zéàè]+[ ]+){1,3}(?:[A-Z][A-Za-zéàè]+)'
+NAME_RE = r"[A-Z][A-Za-zéàè]+"
+NAMES_RE = r"(?:" + NAME_RE + r"[ ]+){1,3}(?:" + NAME_RE + r")"
 FROM_RE = r"(de\s*:\s*)([^\n]*)"
 PHONE_NUMBER_RE = r"[+]?[(]?[0-9]{2}[)]?[-\s.]?[0-9]?[-\s.]?(?:[0-9][-\s.]?){6,10}[0-9]"
-
+TO_RE = r"(à\s*:)([^\n]*)"
+CC_RE = r"(cc\s*:)([^\n]*)"
 
 
 def parse() -> argparse.Namespace:
@@ -98,14 +104,21 @@ def hash_user(user: str):
 
 def process_mail(mail: str, fd: TextIO, hash_link: dict):
     """
+
     This function take a string, split it in sentences and remove from each sentences all confidential data. Finally,
     sentences are written in fd file after removing sensible information.
-    :param mail: An entire string mail to process. A mail start with "de:Machin" and end with EOF or with another
-    "de:autre_Machin".
-    :param fd: It is the file where mails without confidential data are written.
-    :return:
-    """
 
+    Parameters
+    ----------
+    mail : An entire string mail to process. A mail start with "de:Machin" and end with EOF or with another
+    "de:autre_Machin".
+    fd :It is the file where mails without confidential data are written.
+    hash_link : A dictionary that makes correspondences between hash_string and string.
+
+    Returns A dictionary that makes correspondences between hash_string and string.
+    -------
+
+    """
     # catch phone number
     # Since hash can contain a suite of characteres very similar to phone number, it's better to start with phone_number
     results = re.findall(PHONE_NUMBER_RE, mail, re.IGNORECASE)
@@ -113,7 +126,6 @@ def process_mail(mail: str, fd: TextIO, hash_link: dict):
         mail = re.sub("{}".format(result), ANONYME_NUMBER, mail)
 
     # Catch special cases (lines that starts with 'de:','à:'...)
-    # print(re.findall(r"(de\s*:\s*)([^\n]*)", mail, re.IGNORECASE))
     result = re.search(FROM_RE, mail, re.IGNORECASE)
     if result:
         # on récupère le deuxième groupe
@@ -122,47 +134,44 @@ def process_mail(mail: str, fd: TextIO, hash_link: dict):
         hash_link[user_from] = hash_user(user_from)
         mail = re.sub("{}".format(user_from), hash_link[user_from], mail)
 
-    # The receiver have to be hide too. Sincce  there can be several receivers, we should use findall. Findall
-    # return a list of tuple ! There are as many elements in the tuple as there are groups in the regex.
-    results = re.findall(r"(à\s*:)([^\n]*)", mail, re.IGNORECASE)
-    # print(re.findall("(à\\s*:)([^\n]*)", mail, re.IGNORECASE))
+    # The receiver have to be hide too. Since  there can be several receivers, we should use findall.
+    results = re.findall(TO_RE, mail, re.IGNORECASE)
     for result in results:
         user_to = result[1].strip()
         hash_link[user_to] = hash_user(user_to)
         mail = re.sub("{}".format(user_to), hash_link[user_to], mail)
 
     # all people in CC are replaced with ANONYME string
-    result = re.search(r"(cc\s*:)([^\n]*)", mail, re.IGNORECASE)
+    result = re.search(CC_RE, mail, re.IGNORECASE)
     if result:
         cc = result.group(2).strip()
         mail = re.sub("{}".format(cc), ANONYME, mail)
-
-    # an email often finishes with "cordialement, best regards..." and then the name of the sender.
-    result = re.search(END_OF_MAIL, mail, re.IGNORECASE)
-    if result:
-        mail = re.sub(r"{}([A-Z][a-zéà]*\s*)+".format(result.group()),
-                      result.group() + ANONYME + "\n", mail)
 
     # replace all mail by anonymous string. Mail in metadata have already been processed.
     mail = re.sub(REGEX_MAIL, ANONYME, mail)
 
     # Match sensible data in corpus mail
     # match data as monsieur Machin Bidule or Mme. Machine or...
-    # print(re.findall(r"{}\s*([A-Z][a-z]*\s*)+".format(CIVILITE), mail))
-    # si CIVILITE est trop long a remplir, la distance de levenshtein pourrait etre utile
-    mail = re.sub(r"{}\s*([A-Z][a-z]*\s*)+".format(CIVILITE), ANONYME + " ", mail)
+    mail = re.sub(r'\s+{}{}'.format(CIVILITE, NAMES_RE), ANONYME, mail)
+
+    # an email often finishes with "cordialement, best regards..." and then the name of the sender.
+    result = re.search(END_OF_MAIL, mail, re.IGNORECASE)
+    if result:
+        mail = re.sub(r"{}{}".format(result.group(), NAMES_RE),
+                      result.group() + ANONYME + "\n", mail)
 
     # Enfin, on affiche les derniers dont on n'est pas sûr
-    result = re.findall(NAME_RE, mail)
-    print("Les mots suivants n'ont pas été anonymisés et pourtant ce sont peut-être des prénoms :")
-    for maybe_is_name in result:
-        print(re.sub(r'\s', " ", maybe_is_name))
+    result = re.findall(NAMES_RE, mail)
+    if result:
+        print("Les mots suivants n'ont pas été anonymisés et pourtant ce sont peut-être des prénoms :")
+        for maybe_is_name in result:
+            print(re.sub(r'\s', " ", maybe_is_name))
     print("\n")
     fd.write(mail)
     return hash_link
 
 
-def process_file(file_input:TextIO, file_output:TextIO, file_secret:TextIO, hash_dict: dict):
+def process_file(file_input: str, file_output: TextIO, hash_dict: dict):
     """
     This function take a file_input fd descriptor, open it, parse mails in, and process each mail by calling
     process_mail. A mail is a text starting by FROM where FROM is a global variable defined at start at this code.
@@ -194,14 +203,14 @@ def process_file(file_input:TextIO, file_output:TextIO, file_secret:TextIO, hash
         return hash_dict
 
 
-def main(fd_output, fd_secret):
+def main(fd_output: TextIO, fd_secret: TextIO):
     """
     This is the main function. It calls parse function that parses user arguments, check the legality of them and
     anonymize mails contained in file or directory given by user.
     Parameters
     ----------
-    fd_output All the mails anonymized.
-    fd_secret
+    fd_output : File where all mails without sensible data will be written.
+    fd_secret : File where correspondences between string/hash_string will be written
 
     Returns
     -------
@@ -215,26 +224,26 @@ def main(fd_output, fd_secret):
     hash_dict = dict()
 
     if my_args.file:
-        hash_dict = process_file(my_args.file, fd_output, fd_secret, hash_dict)
+        hash_dict = process_file(my_args.file, fd_output, hash_dict)
 
     elif my_args.dir and my_args.rec:
         result = [os.path.join(dp, f) for dp, dn, filenames in os.walk(my_args.dir) for f in filenames]
         for file in result:
-            hash_dict = process_file(file, fd_output, fd_secret, hash_dict)
+            hash_dict = process_file(file, fd_output, hash_dict)
 
     elif my_args.dir:
         files = os.listdir(my_args.dir)
         for file in files:
-            hash_dict = process_file(file, fd_output, fd_secret, hash_dict)
+            hash_dict = process_file(file, fd_output, hash_dict)
 
     for key, value in hash_dict.items():
         fd_secret.write('{}:{}\n'.format(key, value))
 
 
 if __name__ == "__main__":
-    fd_output = open("output", mode='w', encoding=ENCODE_WRITE)
-    fd_secret = open("secret", mode='w', encoding=ENCODE_WRITE)
+    fid_output = open("output", mode='w', encoding=ENCODE_WRITE)
+    fid_secret = open("secret", mode='w', encoding=ENCODE_WRITE)
     # execute only if run as a script
-    main(fd_output, fd_secret)
-    fd_secret.close()
-    fd_output.close()
+    main(fid_output, fid_secret)
+    fid_secret.close()
+    fid_output.close()
