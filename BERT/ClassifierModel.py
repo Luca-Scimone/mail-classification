36f04a17949cbedc21356fa7b7982ec96a587b82
CASIMIRO-MATIAS-MAIA-GIGA GONCALO <goncalo.casimiro-matias-maia-giga@etu.unistra.fr>
2021-05-07 23:45:09 +0200

    bert classifier (doesn't work yet)

--- BERT/ClassifierModel.py
+++ BERT/ClassifierModel.py
@@ -1,15 +1,233 @@
-from Bert import Bert
+from Bert import load_data
+
+import os
+import json
+import time
 import torch
+import datetime
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from torch.utils.data import TensorDataset, random_split, \
+                            DataLoader, RandomSampler, SequentialSampler
+from transformers import CamembertForSequenceClassification, CamembertTokenizer, \
+                         AdamW, get_linear_schedule_with_warmup
 
-# Initialization of Bert
-bert = Bert ()
-bert.__init_bert_tools__()
+df = load_data("./data/mails", "utf-8", False)
 
-# Creating tensors from the padded matrix and the mask
-input_ids = torch.tensor(bert.padded)  
-attention_mask = torch.tensor(bert.attention_mask)
+use_X_mails = 5
+df = df.iloc[: , :use_X_mails]
 
-# Fetching the features
-with torch.no_grad():
-    last_hidden_states = bert.model(input_ids, attention_mask=attention_mask)
-features = last_hidden_states[0][:,0,:].numpy()
+mails = df.iloc[0].values.tolist()
+labels = df.iloc[1].values.tolist()
+
+TOKENIZER = CamembertTokenizer.from_pretrained(
+    'camembert-base',
+    do_lower_case=True)
+
+### ========== Encodage ========== ###
+
+def preprocess(raw_mails, labels=None):
+    encoded_batch = TOKENIZER.batch_encode_plus(raw_mails,
+                                                add_special_tokens=True,
+                                                truncation = True,
+                                                max_length = 511,
+                                                pad_to_max_length=True,
+                                                return_attention_mask=True,
+                                                return_tensors = 'pt')
+    if labels:
+        labels = torch.tensor(labels)
+        return encoded_batch['input_ids'], encoded_batch['attention_mask'], labels
+    return encoded_batch['input_ids'], encoded_batch['attention_mask']
+
+
+# Split train-validation
+split_border = int(len(labels)*0.8)
+mails_train, mails_validation = mails[:split_border], mails[split_border:]
+labels_train, labels_validation = labels[:split_border], labels[split_border:]
+
+input_ids, attention_mask, labels_train = preprocess(mails_train, labels_train)
+# Combine the training inputs into a TensorDataset
+train_dataset = TensorDataset(
+    input_ids,
+    attention_mask,
+    labels_train)
+
+input_ids, attention_mask, labels_validation = preprocess(mails_validation, labels_validation)
+# Combine the validation inputs into a TensorDataset
+validation_dataset = TensorDataset(
+    input_ids,
+    attention_mask,
+    labels_validation)
+
+# size of 16 or 32.
+batch_size = 16
+
+# Create the DataLoaders
+train_dataloader = DataLoader(
+            train_dataset,
+            sampler = RandomSampler(train_dataset),
+            batch_size = batch_size)
+
+validation_dataloader = DataLoader(
+            validation_dataset,
+            sampler = SequentialSampler(validation_dataset),
+            batch_size = batch_size)
+
+model = CamembertForSequenceClassification.from_pretrained(
+        'camembert-base',
+        num_labels = 6)
+
+
+def predict(labels, model=model):
+    with torch.no_grad():
+        model.eval()
+        input_ids, attention_mask = preprocess(labels)
+        retour = model(input_ids, attention_mask=attention_mask)
+        print(retour)
+        return torch.argmax(retour[0], dim=1)
+
+
+def evaluate(mails, labels, metric='report'):
+    predictions = predict(mails)
+    if metric == 'report':
+        return metrics.classification_report(labels, predictions, zero_division=0)
+    elif metric == 'matrix':
+        return metrics.confusion_matrix(labels, predictions)
+
+
+def format_time(elapsed):
+    # Round to the nearest second.
+    elapsed_rounded = int(round((elapsed)))
+    
+    return str(datetime.timedelta(seconds=elapsed_rounded))
+
+
+optimizer = AdamW(model.parameters(),
+                  lr = 2e-5, # Learning Rate - Default is 5e-5
+                  eps = 1e-8 # Adam Epsilon  - Default is 1e-8.
+                )
+
+
+# Training loop
+training_stats = []
+                                                                                
+# Measure the total training time for the whole run.
+total_t0 = time.time()
+
+epochs = 3
+
+# Total number of training steps is [number of batches] x [number of epochs]
+# (Note that this is not the same as the number of training samples)
+total_steps = len(train_dataloader) * epochs
+
+# Create the learning rate scheduler
+scheduler = get_linear_schedule_with_warmup(optimizer, 
+                                            num_warmup_steps = 0, # Default value in run_glue.py
+                                            num_training_steps = total_steps)
+
+# This variable will evaluate the convergence on the training
+consecutive_epochs_with_no_improve = 0
+
+# Training
+for epoch in range(0, epochs):
+    
+    print("")
+    print(f'########## Epoch {epoch} / {epochs} ##########')
+    print('Training...')
+
+    # Measure how long the training epoch takes.
+    t0 = time.time()
+
+    # Reset the total loss for this epoch.
+    total_train_loss = 0
+
+    # Put the model into training mode
+    model.train()
+
+    # For each batch of training data
+    for step, batch in enumerate(train_dataloader):
+
+        # Progress update every 40 batches.
+        if step % 40 == 0 and not step == 0:
+            # Calculate elapsed time in minutes.
+            elapsed = time.time() - t0
+            
+            # Report progress
+            print(f'  Batch {step}  of  {len(train_dataloader)}    Elapsed: {format_time(elapsed)}.')
+
+        input_id = batch[0]
+        attention_mask = batch[1]
+        label = batch[2]
+
+        # Clear any previously calculated gradients before performing a backward pass
+        model.zero_grad()        
+
+        # Perform a forward pass (evaluate the model on this training batch)
+        # the loss (because we provided skills) and the "logits"--the model
+        # outputs prior to activation
+        out = model(input_id, 
+                    token_type_ids=None, 
+                    attention_mask=attention_mask, 
+                    labels=label)
+
+        loss, logits = out[0], out[1]
+
+        # Accumulate the training loss over all of the batches so that we can
+        # calculate the average loss at the end. 'loss' is a Tensor containing a
+        # single value; the '.item()' function just returns the Python value 
+        # from the tensor
+        total_train_loss += loss.item()
+
+        # Perform a backward pass to calculate the gradients
+        loss.backward()
+
+        # Clip the norm of the gradients to 1.0
+        # This is to help prevent the "exploding gradients" problem.
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+        # Update parameters and take a step using the computed gradient
+        # The optimizer dictates the "update rule"--how the parameters are
+        # modified based on their gradients, the learning rate, etc.
+        optimizer.step()
+
+        # Update the learning rate.
+        scheduler.step()
+
+    # Calculate the average loss over all of the batches
+    avg_train_loss = total_train_loss / len(train_dataloader)   
+
+    if epoch > 0:
+        if min([stat['Training Loss'] for stat in training_stats]) <= avg_train_loss:
+            # i.e. If there is not improvement
+            consecutive_epochs_with_no_improve += 1
+        else:
+            # If there is improvement
+            consecutive_epochs_with_no_improve = 0
+    
+    # Measure how long this epoch took
+    training_time = time.time() - t0
+
+    print("")
+    print("  Average training loss: {0:.2f}".format(avg_train_loss))
+    print("  Training epoch took: {:}".format(training_time))
+    
+    # Record all statistics from this epoch.
+    training_stats.append(
+        {
+            'epoch': epoch + 1,
+            'Training Loss': avg_train_loss,
+            'Training Time': training_time,
+        }
+    )
+    if consecutive_epochs_with_no_improve == 2:
+        print("Stop training : The loss has not changed since 2 epochs!")
+        break
+
+# Evaluation with the confusion matrix
+import seaborn
+confusion_matrix = evaluate(mails_validation, labels_validation, 'matrix')
+report = evaluate(mails_validation, labels_validation, 'report')
+print(report)
+seaborn.heatmap(confusion_matrix)
