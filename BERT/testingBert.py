1d0a0e8381dfcbeebb07faef2e9eff6b964e4e05
CASIMIRO-MATIAS-MAIA-GIGA GONCALO <goncalo.casimiro-matias-maia-giga@etu.unistra.fr>
2021-05-07 17:42:52 +0200

    organizing BERT work

---
+++ BERT/testingBert.py
@@ -0,0 +1,24 @@
+from Bert import Bert
+import seaborn as sns
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix, accuracy_score
+import matplotlib.pyplot as plt
+
+X_train, X_test, y_train, y_test = Bert().embedding()
+
+label_names = ['Déménagement', 'Relève de compteur', 'Réclamation',
+    'Contrat – Coordonnées personnelles', 'Facture – Paiement', 'Espace client']
+
+### --- Testing BERT feature extraction with Gaussian Naive Bayes --- ###
+gnb = GaussianNB()
+y_pred = gnb.fit(X_train, y_train).predict(X_test).astype('int')
+
+print (accuracy_score(y_test, y_pred))
+
+# Ploting the confusion matrix
+mat = confusion_matrix(y_pred, y_test)
+sns.heatmap(mat.T, square = True, annot=True, fmt = "d", xticklabels=label_names,yticklabels=label_names)
+plt.title("Using Bayes Classification")
+plt.xlabel("predicted label")
+plt.ylabel("true labels")
+plt.show()
