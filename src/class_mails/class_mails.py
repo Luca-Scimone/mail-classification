31589b24b42cfd69857d23f71840d330637ebf4a
CORIAT BASTIEN <bastien.coriat@etu.unistra.fr>
2021-09-16 15:38:48 +0200

    Implement pipelines

---
+++ src/class_mails/class_mails.py
@@ -0,0 +1,23 @@
+class Mails:
+    def __init__(self):
+        self.mails = dict() # dict of Mails
+
+    def get_mails(self):
+        return self.data
+
+    def read_mails(self, path: str):
+        pass
+
+
+class Mail:
+    """
+
+    """
+    def label(self):
+        pass
+
+    def message(self):
+        pass
+
+    def object(self):
+        pass
--- src/pipelines.py
+++ src/pipelines.py
@@ -1,5 +1,35 @@
+from src.class_mails.class_mails import Mails
+from sklearn.pipeline import Pipeline
+
 class PipelinesManager:
 
-    def __init__(self):
+    def __init__(self, path=""):
+        self.classifiers = dict()
+        self.data = Mails()
+        self._read_data(path=path)
+
+    def _read_data(self):
+        self.data.read_mails()
+
+    def add_pipeline(self):
+        return Pipeline()
+
+class Pipeline:
+    def __init__(self, data, parameters={}):
+        self.data = data
+        self.parameters = parameters
+        self.pipeline = None
+
+    def make_pipeline(self, list):
+        self.pipeline =
+
+    def fit(self):
         pass
+
+    def transform(self):
+        pass
+
+
+
+
 
