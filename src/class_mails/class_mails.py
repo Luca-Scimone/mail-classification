71c945c6e794a303adfa588af4d50fa530cb4650
CASIMIRO-MATIAS-MAIA-GIGA GONCALO <goncalo.casimiro-matias-maia-giga@etu.unistra.fr>
2021-09-22 09:17:50 +0200

    Starting a pipeline

--- src/class_mails/class_mails.py
+++ src/class_mails/class_mails.py
@@ -13,17 +13,25 @@ class Data(ABC):
 
     @abstractmethod
     def get(self):
+        """
+        An abstract method for the getter of the class Data
+        """
         pass
-
-    """
-    Set your mails from a stream (file, tcp connexion, other object...)
-    """
 
     @abstractmethod
-    def set(self, encoding, header, stream=None):
+    def set(self, stream, encoding, header):
+        """
+        An abstract method for the setter of the class Data
+        """
         pass
 
+
 class Mails(Data):
+    """
+    A class Mails containing all methods to extract information from a csv file
+    where mails are stored.
+    """
+
     def __init__(self):
         # list of Mails
         self._mails = []
@@ -51,20 +59,24 @@ class Mails(Data):
             ]
 
     def __str__(self):
-        r = ""
+        """
+        Overload of str in order to print mails more clearly
+        """
+
+        res = ""
         for mail in self.mails:
-            r += "\n\n" + mail.__str__()
-        return r
+            res += "\n\n" + mail.__str__()
+        return res
 
     def get(self):
         return self._mails
 
-    """
-    Reads a csv file of path 'stream'. Each column should represent the types
-    defined in self._row_names.
-    """
-
     def set(self, stream, encoding, header):
+        """
+        Reads a csv file of path 'stream'. Each column should represent the
+        types defined in self._row_names.
+        """
+
         if not os.path.exists(stream):
             raise Exception("Missing or invalid path to data.")
 
@@ -86,8 +98,8 @@ class Mails(Data):
                     if col_idx < len(self._row_names):
                         temp_dict[self._row_names[col_idx]] = col
                     else:
-                        print("[W] Ignored column %d in mail %d of %s (extra column)"
-                              % (col_idx, mail_cnt, stream))
+                        print("""[W] Ignored column %d in mail %d of %s
+                              (extra column)""" % (col_idx, mail_cnt, stream))
                     col_idx += 1
 
                 if col_idx < len(self._row_names):
@@ -101,19 +113,20 @@ class Mails(Data):
     def mails(self) -> list:
         return self._mails
 
-    """ 
-    Read mails from path and put them in a dict (self.mails).
-    """
-
     @mails.setter
     def mails(self, path: str):
+        """
+        Read mails from path and put them in a dict (self.mails).
+        """
         read_mails = []
         self._mails = read_mails
 
 
 class Mail:
-
-    # A Mail is internally a dictionary.
+    """
+    A class Mail representing a dictionary where each section of the mail is
+    given (CC, CCi ect..)
+    """
 
     def __init__(self, fields_mail: dict):
         self.fields_mail = fields_mail
@@ -121,67 +134,59 @@ class Mail:
     def __str__(self):
         return self.fields_mail.__str__()
 
-    """ 
-    Set a label to a particular mail. This function should never been used. It
-    is only needed when we want to write mails with label on disk. The
-    PipelinesManager should never called set_label. 
-    """
-
     def set_label(self, label: list):
+        """
+        Set a label to a particular mail. This function should never been used.
+        It is only needed when we want to write mails with label on disk. The
+        PipelinesManager should never called set_label.
+        """
         self.fields_mail["label"] = label
 
-    """
-        Return an iterator over labels of a mail. For example [0,1,0...]  
-    """
-
     def label(self) -> list:
+        """
+        Return an iterator over labels of a mail. For example [0,1,0...]
+        """
         return self.fields_mail["label"]
 
-    """
-        Split labels in multiple lists. For example [1,0,1,0] become
-        ([1,0,0,0], [0,0,1,0]). 
-    """
-
     def labels(self) -> list:
+        """
+        Split labels in multiple lists. For example [1,0,1,0] become
+        ([1,0,0,0], [0,0,1,0]).
+        """
         pass
-
-    """
-    Return the position of the first label. [0,0,0,1,1] gives 3. 
-    """
 
     def get_first_label(self) -> int:
+        """
+        Return the position of the first label. [0,0,0,1,1] gives 3.
+        """
         pass
 
-    """ 
-    Return the message of the mail as a string.
-    """
-
     def message(self) -> str:
+        """
+        Return the message of the mail as a string.
+        """
         return self.fields_mail["message"]
 
-    """ 
-        Return the object of the mail as a string.
-    """
-
     def object(self) -> str:
+        """
+        Return the object of the mail as a string.
+        """
         return self.fields_mail["object"]
 
-    """
-    Transform a mail to a dict where value are array. It's only useful for the to_dataframe method. 
-    """
-
     def _to_dict_array(self):
+        """
+        Transform a mail to a dict where value are array. It's only useful for
+        the to_dataframe method.
+        """
         for key, value in self.fields_mail.items():
             self.fields_mail[key] = [value]
         return self.fields_mail
 
-    """
-    Return a mail as a pandas dataframe. For example : 
-       de   cc   cci   A   objet                  corps
-    0  Na   Na   Naa   N   Na            un mail random
-    1  Na   Na   Naa   N   Na      Un autre mail random
-    """
-
     def to_dataframe(self) -> DataFrame:
+        """
+        Return a mail as a pandas dataframe. For example :
+        de   cc   cci   A   objet                  corps
+        0  Na   Na   Naa   N   Na            un mail random
+        1  Na   Na   Naa   N   Na      Un autre mail random
+        """
         return pd.DataFrame.from_dict(self._to_dict_array())
-
--- src/gonçalo_main.py
+++ src/gonçalo_main.py
@@ -1,4 +1,6 @@
 from estimators.example import ExampleEstimator, Example2Estimator, FirstEstimator
+from estimators.preprocessing import Lemme
+from estimators.preprocessing import ToDataFrame
 from example_pipelines.pipelines_example import SVM_Pipeline, EmptyPipeline
 from pipelines import PipelinesManager
 
@@ -7,15 +9,12 @@ if __name__ == "__main__":
     # some tasks
     manager = PipelinesManager("mails.csv")
 
-    # First example, you can instantiate a high pipeline_example
-    svm_pipeline = SVM_Pipeline()
-    svm_pipeline.shared_data = manager.data  # set data for training
-    svm_pipeline.demo()  # Run your demo defined in svm_pipeline
+    pret_pipeline = EmptyPipeline()
 
-    # Maybe you want to define your pipeline. It is not the best way to do so. You can rather inherited
-    # Empty_Pipeline in pipelines_example. This main must stay as short as possible.
-    my_pipeline = EmptyPipeline()
-    my_pipeline.shared_data = manager.data  # set data for training
-    my_pipeline.pipeline = (FirstEstimator(), ExampleEstimator(), Example2Estimator())  # Give your pipeline
-    my_pipeline.transform()
-    my_pipeline.show_confusion_matrix()
+    pret_pipeline.shared_data = manager.data
+
+    pret_pipeline.pipeline = [ToDataFrame()]
+
+    out = pret_pipeline.transform()
+
+    print(out)
